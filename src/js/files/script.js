ff7da654d96e292493881f3c46cd10db927eb058
LayMorja (MateBook) <evil-corporaty@yandex.ru>
2023-06-20 20:13:49 +0300

    Slick added

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,13 @@
 // import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 // import { flsModules } from "./modules.js";
+
+$('.examples__slider').slick({
+  slidesToShow: 1,
+  nextArrow: '.examples__arrow-next',
+  prevArrow: '.examples__arrow-prev',
+  dots: false,
+  variableWidth: true,
+  infinite: true,
+  mobileFirst: true,
+});
